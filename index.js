bbc729419dcdd7855922459752843331f293f298
FX <danielfx011@gmail.com>
2024-09-16 17:32:41 +0100

    fix

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const path = require("path");
 const config = require("./config");
-const { connect, modulesJS } = require("./lib");
+const { connect, modulesJS, sleep } = require("./lib");
 const { fetchPlugins } = require("./lib/db/plugins");
 const app = express();
 const PORT = 8000;
 
 async function initialize() {
 	try {
+		await sleep(2000);
 		await modulesJS(path.join(__dirname, "/lib/db/"));
 		await config.DATABASE.sync();
 		await modulesJS(path.join(__dirname, "/plugins/"));
