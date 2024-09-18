5d684d907f2a4a0ae30c8d536cd14f1e3599f8dc
FX <danielfx011@gmail.com>
2024-09-18 20:27:18 +0100

    fix

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const { connect, modulesJS, sleep, fetchPlugins } = require("./lib");
-const express = require("express");
-const path = require("path");
-const config = require("./config");
-const io = require("socket.io-client");
-const app = express();
-const PORT = 8000;
+const StartApp = require("./lib/app");
 
-async function initialize() {
-	await sleep(2000);
-	await modulesJS(path.join(__dirname, "/lib/db/"));
-	await config.DATABASE.sync();
-	await modulesJS(path.join(__dirname, "/plugins/"));
-	await fetchPlugins();
-	console.log("Modules Installed");
-	const ws = io("https://socket-counter-yb8q.onrender.com/", { reconnection: true });
-	ws.on("connect", () => console.log("Connected to server"));
-	ws.on("disconnect", () => console.log("Disconnected from server"));
-	return await connect();
-}
-
-initialize();
-
-app.listen(PORT);
+const appInit = new StartApp();
+appInit.initialize().then(() => {
+	return true;
+});
