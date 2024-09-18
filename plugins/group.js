02d081641ad7ed3224a5f7baf6595440c6fa8326
FX <danielfx011@gmail.com>
2024-09-18 16:29:32 +0100

    fix

--- plugins/group.js
+++ plugins/group.js
@@ -1,5 +1,166 @@
 const { Module, parsedJid, isAdmin } = require("../lib/");
 const { banUser, unbanUser, isBanned } = require("../lib/db/ban");
+const { setMessage, getMessage, delMessage, getStatus, toggleStatus } = require("../lib/db").Greetings;
+const { getAntilink, updateAntilink, createAntilink } = require("../lib/db/antilink");
+
+Module(
+	{
+		pattern: "antilink",
+		fromMe: mode,
+		desc: "Manage antilink settings",
+		type: "group",
+	},
+	async (message, match) => {
+		const groupId = message.jid;
+		let antilink = await getAntilink(groupId);
+		const [action] = match.split(" ");
+
+		switch (action) {
+			case "on":
+				if (!antilink) {
+					antilink = await createAntilink(groupId);
+				}
+				await updateAntilink(groupId, { isEnabled: true });
+				await message.reply("Antilink has been enabled for this group.");
+				break;
+
+			case "off":
+				if (antilink) {
+					await updateAntilink(groupId, { isEnabled: false });
+					await message.reply("Antilink has been disabled for this group.");
+				} else {
+					await message.reply("Antilink is not set up for this group.");
+				}
+				break;
+
+			case "kick":
+				if (!antilink) {
+					antilink = await createAntilink(groupId);
+				}
+				await updateAntilink(groupId, { action: "kick" });
+				await message.reply("Antilink action set to: kick");
+				break;
+
+			case "all":
+				if (!antilink) {
+					antilink = await createAntilink(groupId);
+				}
+				await updateAntilink(groupId, { action: "all" });
+				await message.reply("Antilink action set to: all");
+				break;
+
+			case "get":
+				if (antilink) {
+					await message.reply(`Antilink status:
+Enabled: ${antilink.isEnabled}
+Action: ${antilink.action}`);
+				} else {
+					await message.reply("Antilink is not set up for this group.");
+				}
+				break;
+
+			default:
+				await message.reply(`Usage:
+.antilink on - Enable antilink
+.antilink off - Disable antilink
+.antilink kick - Set action to kick
+.antilink all - Set action to all (kick, warn, delete)
+.antilink get - Check antilink status`);
+		}
+	},
+);
+
+Module(
+	{
+		pattern: "welcome",
+		fromMe: true,
+		desc: "description",
+		type: "group",
+	},
+	async (message, match) => {
+		if (!message.isGroup) return;
+		let { prefix } = message;
+		let status = await getStatus(message.jid, "welcome");
+		let stat = status ? "on" : "off";
+
+		if (!match) {
+			let replyMsg = `Welcome manager\n\nGroup: ${(await message.client.groupMetadata(message.jid)).subject}\nStatus: ${stat}\n\nAvailable Actions:\n\n- ${prefix}welcome get: Get the welcome message\n- ${prefix}welcome on: Enable welcome message\n- ${prefix}welcome off: Disable welcome message\n- ${prefix}welcome delete: Delete the welcome message`;
+
+			return await message.reply(replyMsg);
+		}
+
+		if (match === "get") {
+			let msg = await getMessage(message.jid, "welcome");
+			if (!msg) return await message.reply("_There is no welcome set_");
+			return message.reply(msg.message);
+		}
+
+		if (match === "on") {
+			let msg = await getMessage(message.jid, "welcome");
+			if (!msg) return await message.reply("_There is no welcome message to enable_");
+			if (status) return await message.reply("_Welcome already enabled_");
+			await toggleStatus(message.jid);
+			return await message.reply("_Welcome enabled_");
+		}
+
+		if (match === "off") {
+			if (!status) return await message.reply("_Welcome already disabled_");
+			await toggleStatus(message.jid, "welcome");
+			return await message.reply("_Welcome disabled_");
+		}
+
+		if (match == "delete") {
+			await delMessage(message.jid, "welcome");
+			return await message.reply("_Welcome deleted successfully_");
+		}
+		await setMessage(message.jid, "welcome", match);
+		return await message.reply("_Welcome set successfully_");
+	},
+);
+
+Module(
+	{
+		pattern: "goodbye",
+		fromMe: true,
+		desc: "description",
+		type: "group",
+	},
+	async (message, match) => {
+		if (!message.isGroup) return;
+		let status = await getStatus(message.jid, "goodbye");
+		let stat = status ? "on" : "off";
+		let replyMsg = `Goodbye manager\n\nGroup: ${(await message.client.groupMetadata(message.jid)).subject}\nStatus: ${stat}\n\nAvailable Actions:\n\n- goodbye get: Get the goodbye message\n- goodbye on: Enable goodbye message\n- goodbye off: Disable goodbye message\n- goodbye delete: Delete the goodbye message`;
+
+		if (!match) {
+			return await message.reply(replyMsg);
+		}
+
+		if (match === "get") {
+			let msg = await getMessage(message.jid, "goodbye");
+			if (!msg) return await message.reply("_There is no goodbye set_");
+			return message.reply(msg.message);
+		}
+
+		if (match === "on") {
+			await toggleStatus(message.jid, "goodbye");
+			return await message.reply("_Goodbye enabled_");
+		}
+
+		if (match === "off") {
+			await toggleStatus(message.jid);
+			return await message.reply("_Goodbye disabled_");
+		}
+
+		if (match === "delete") {
+			await delMessage(message.jid, "goodbye");
+			return await message.reply("_Goodbye deleted successfully_");
+		}
+
+		await setMessage(message.jid, "goodbye", match);
+		return await message.reply("_Goodbye set successfully_");
+	},
+);
+
 Module(
 	{
 		on: "message",
