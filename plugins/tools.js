d70ad9f53331c8192f8592e21b2c16df71cef446
FX <danielfx011@gmail.com>
2024-09-18 16:13:41 +0100

    trim

--- plugins/tools.js
+++ plugins/tools.js
@@ -1,4 +1,5 @@
-const { Module, mode, qrcode, isUrl, Bitly, removeBg, tinyurl, ssweb, shortenurl, upload, IronMan } = require("../lib");
+const { Module, mode, qrcode, isUrl, Bitly, removeBg, tinyurl, ssweb, shortenurl, upload, IronMan, ffmpeg, parseTimeToSeconds } = require("../lib");
+const { fromBuffer } = require("file-type");
 const config = require("../config");
 
 Module(
@@ -177,5 +178,31 @@ Module(
 		if (!message.quoted) return message.reply("_*Reply to a user*_");
 		let sender = "https://wa.me/" + (message.reply_message.sender || message.mention[0] || message.text).split("@")[0];
 		await message.reply(sender);
+	},
+);
+
+Module(
+	{
+		pattern: "trim",
+		fromMe: mode,
+		desc: "Trim the video or audio",
+		type: "tools",
+	},
+	async (message, match, m) => {
+		if (!message.reply_message || (!message.reply_message.video && !message.reply_message.audio)) {
+			return await message.sendMessage("Reply to a media file");
+		}
+		if (!match) return await message.sendMessage("Give the start and end time in this format: mm:ss|mm:ss");
+
+		const [start, end] = match.split("|");
+		if (!start || !end) return await message.sendMessage("Give the start and end time in this format: mm:ss|mm:ss");
+		const buffer = await m.quoted.download();
+		const startSeconds = parseTimeToSeconds(start);
+		const endSeconds = parseTimeToSeconds(end);
+		const duration = endSeconds - startSeconds;
+		const ext = (await fromBuffer(buffer)).ext;
+		const args = ["-ss", `${startSeconds}`, "-t", `${duration}`, "-c", "copy"];
+		const trimmedBuffer = await ffmpeg(buffer, args, ext, ext);
+		message.sendFile(trimmedBuffer);
 	},
 );
